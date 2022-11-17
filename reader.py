557197861f8ea5b91cf0e16369e16ac17211131e
Patryk Konopka <konopka90@gmail.com.com>
2022-11-17 16:17:58 +0100

    Some cleanup

--- reader.py
+++ reader.py
@@ -1,12 +1,10 @@
-from sys import argv
 import os
-from time import sleep
-import smbus2
-import bme280
 import random
-import sys
 from datetime import datetime
+from time import sleep
 
+import bme280
+import smbus2
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
@@ -15,10 +13,10 @@ temperature = 0.0
 humidity = -1
 pressure = -1
 
-# You can generate a Token from the "Tokens Tab" in the UI
-token = "cJ6YQc3-8I43zA6V6tns5adjjCV0-176kWEoRhIolxVdjArSpYYrUaZ0Bv7_oJSP4tQhaG7RyiOb-ZJ2rG0iqw=="
-org = "pi"
-bucket = "pi"
+TOKEN = "cJ6YQc3-8I43zA6V6tns5adjjCV0-176kWEoRhIolxVdjArSpYYrUaZ0Bv7_oJSP4tQhaG7RyiOb-ZJ2rG0iqw=="
+ORG = "pi"
+BUCKET = "pi"
+
 url = os.environ.get('INFLUXDB_URL', 'http://localhost:8086')
 interval = int(os.environ.get('POLLING_INTERVAL', 5))
 sim = os.environ.get('SIMULATION_MODE', 'no') == 'yes'
@@ -27,11 +25,11 @@ device_address = int(os.environ.get('DEVICE_ADDRESS', '0x76'), 0)
 is_ok_counter = 0
 
 print('* Configuration *')
-print('INFLUXDB_URL={}'.format(url))
-print('SIMULATION_MODE={}'.format(sim))
-print('POLLING_INTERVAL={}'.format(interval))
-print('DEVICE_PORT={}'.format(device_port))
-print('DEVICE_ADDRESS={}'.format(device_address))
+print(f'INFLUXDB_URL={url}')
+print(f'SIMULATION_MODE={sim}')
+print(f'POLLING_INTERVAL={interval}')
+print(f'DEVICE_PORT={device_port}')
+print(f'DEVICE_ADDRESS={device_address}')
 
 while True:
     try:
@@ -45,15 +43,13 @@ while True:
             calibration_params = bme280.load_calibration_params(
                 bus, device_address)
 
-            # the sample method will take a single reading and return a
-            # compensated_reading object
             data = bme280.sample(bus, device_address, calibration_params)
-
+            
             temperature = data.temperature
             pressure = data.pressure
             humidity = data.humidity
 
-        with InfluxDBClient(url=url, token=token) as client:
+        with InfluxDBClient(url=url, token=TOKEN) as client:
             with client.write_api(write_options=SYNCHRONOUS) as write_api:
                 point = Point("mem")\
                     .tag("host", "pi")\
@@ -62,7 +58,7 @@ while True:
                     .field("humidity", humidity)\
                     .time(datetime.utcnow(), WritePrecision.NS)
 
-                write_api.write(bucket, org, point)
+                write_api.write(BUCKET, ORG, point)
 
         is_ok_counter = is_ok_counter + 1
         if is_ok_counter == 1:
@@ -77,3 +73,4 @@ while True:
         print("Unknown exception: " + str(e))
 
     sleep(interval)
+
